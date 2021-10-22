d8d2788bac6753a997c13554c0a068dc83fe69b8
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-22 16:56:01 +0100

    finished pokemon controller

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,25 +1,41 @@
 class PokemonsController < ApplicationController
   # Had to call it Pokemons controller to be consistent with Rails convention :sadge:
   def index
+    # index page for all Pokemon - should be a long list just like on Smogon
     @pokemons = Pokemon.all
   end
 
   def show
+    # show page for a specific Pokemon
     @pokemon = Pokemon.find[params[:id]]
   end
 
   def new
+    # build new instance of Pokemon (not that you would ever need to)
+    @pokemon = Pokemon.new
   end
 
   def create
+    # don't know why would need this unless a new pokemon gets released or you want to make a new one
+    @pokemon = Pokemon.new(pokemon_params)
+    @pokemon.save
   end
 
   def edit
+    # don't need
   end
 
   def update
+    # don't need
   end
 
   def destroy
+    # don't need
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :number, :entry, :height, :weight, :hp, :atk, :def, :spatk, :spdef, :speed, :type_one, :type_two)
   end
 end
