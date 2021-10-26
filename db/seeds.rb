c5283928326d493dbcdaf846808dff931c570954
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-26 17:01:38 +0100

    setting up seeding of Pokemon into database with the pokeapi gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# requiring poke-api gem
+require 'poke-api-v2'
+
+# looping through all the pokemon and creating them, adding them to the DB
+for i in 1..890 do
+  PokeApi.get(id: i)
+  Pokemon.create()
+end
